2da72edb099b7dcb56b8c01aa3c3a96e4cabbe6e
JEMZXCjemzxc <jeremeflores5@gmail.com>
2025-04-27 13:24:03 +0800

    fist commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template # type: ignore
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
